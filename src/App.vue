dee967504534f9d4ff75e3cdf81a41718b3e0f9a
Daniel Briskin <daniel.briskin@gmail.com>
2019-06-07 20:16:40 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,53 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="doThing">Click me</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import axiosCookieJarSupport from '@3846masa/axios-cookiejar-support';
+import tough from 'tough-cookie';
 import HelloWorld from './components/HelloWorld.vue';
+
+axiosCookieJarSupport(axios);
+
+const cookieJar = new tough.CookieJar();
+
+const baseURL = 'http://localhost:8081/';
+const apiUser = '';
+const apiPassword = '';
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+  },
+  methods: {
+    async doThing() {
+      console.log('hin');
+      try {
+        let result = await axios.post(`${baseURL}api/v0/login`,
+          {
+            email: apiUser,
+            password: apiPassword,
+          },
+          {
+            jar: cookieJar, // tough.CookieJar or boolean
+            withCredentials: true, // If true, send cookie stored in jar
+          });
+        console.log(result);
+        result = await axios.get(`${baseURL}api/v0/chains/ethereum/contracts/zombiefactory/addresses/zombiefactory/owner`,
+          {
+            jar: cookieJar,
+            withCredentials: true,
+          });
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+      }
+    },
   },
 };
 </script>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  devServer: {
+    proxy: 'https://pryum.rink.decrypt.multibaas.com/'
+  },
+};
