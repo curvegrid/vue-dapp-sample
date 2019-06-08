d3ec4703a4ee768748109be8eaac60cb5ba197ad
Daniel Briskin <daniel.briskin@gmail.com>
2019-06-08 17:47:27 +0900

    even better

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   devServer: {
     https: true,
+    host: 'localhost',
+    port: 8080,
     proxy: {
       '/': {
+        ws: false,
         target: 'https://octo4.rink.decrypt.multibaas.com/',
         changeOrigin: true,
         cookieDomainRewrite: 'localhost',
